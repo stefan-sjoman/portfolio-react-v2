439ed12d4eaf7bbd7ac9cb414978cd4e86c0ad40
Stefan Sjöman <stefan.sjoman@hotmail.com>
2021-08-10 09:51:37 +0200

    first git, cleaned template

---
+++ src/components/Header-Comp.jsx
@@ -0,0 +1,17 @@
+
+const HeaderComp = () => {
+	return (
+		<header>
+			<h1>Stefan Sjöman</h1>
+			<nav>
+				<ul>
+					<li>Portfolio</li>
+					<li>Education</li>
+					<li>Experience</li>
+				</ul>
+			</nav>
+		</header>
+	);
+}
+
+export default HeaderComp;
