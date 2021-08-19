3bdb4abb9a31e16b55710cb3812b519d46c31719
Stefan Sjöman <stefan.sjoman@hotmail.com>
2021-08-19 11:15:36 +0200

    made new logo and added description to portfolio links.

--- src/components/PortfolioComp.jsx
+++ src/components/PortfolioComp.jsx
@@ -13,13 +13,30 @@ const PortfolioComp = () => {
 
 	return (
 		<div className="portfolio-comp tone-in">
-			<a href="https://sjoman-grocery-list.herokuapp.com/" rel="noreferrer" target="_blank">Grocery List</a>
-			<a href="https://sjoman-hamsterwars-fullstack.herokuapp.com/" rel="noreferrer" target="_blank">Hamsterwars</a>
-			<a href="portfolio/Unsplash/index.html" target="_blank">Unsplash Images</a>
-			<a href="portfolio/StarWarsV4/index.html" target="_blank">Star Wars</a>
-			<a href="portfolio/BeerProject/index.html" target="_blank">Beer Project</a>
-			<a href="portfolio/InsuranceProject/index.html" target="_blank">Insurance Project</a>
-			<a href="portfolio/TärningV3/index.html" target="_blank">eDice</a>
+			<a href="https://sjoman-grocery-list.herokuapp.com/" rel="noreferrer" target="_blank">
+				<h2>Grocery List</h2>
+				<p>App for users to add groceries to the same list. Fullstack React app with Firebase.</p>
+			</a>
+			<a href="https://sjoman-hamsterwars-fullstack.herokuapp.com/" rel="noreferrer" target="_blank">
+				<h2>Hamsterwars</h2>
+				<p>Voting app for the cutest hamsters. Fullstack React app with Firebase.</p>
+			</a>
+			<a href="portfolio/Unsplash/index.html" target="_blank">
+				<h2>Unsplash Images</h2>
+				<p>Image search app with images from Unsplash. Vue app with the Unsplash api.</p>
+			</a>
+			<a href="portfolio/StarWarsV4/index.html" target="_blank">
+				<h2>Star Wars</h2>
+				<p>Info app about Star Wars. Vue app with the Star Wars Api, SWAPI.</p>
+			</a>
+			<a href="portfolio/InsuranceProject/index.html" target="_blank">
+				<h2>Insurance Project</h2>
+				<p>First assignment in school. Designed from figma mockup with HTML and CSS.</p>
+			</a>
+			<a href="portfolio/TärningV3/index.html" target="_blank">
+				<h2>eDice</h2>
+				<p>Just a simple dice. Made with JavaScript, HTML and CSS.</p>
+			</a>
 		</div>
 	);
 }
